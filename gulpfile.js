7abc52644ff6969ca5651788c3ba8df0ea0adb28
Thibault Derousseaux <tib.dex@gmail.com>
2015-03-04 16:10:54 +0100

    Adding deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,31 @@ var gulp = require('gulp'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     del = require('del'),
+    rsync = require('gulp-rsync'),
+    runSequence = require('run-sequence'),
     sass = require('gulp-sass'),
     uglify = require('gulp-uglify');
 
-gulp.task('build', ['bundle', 'sass', 'views', 'assets']);
+gulp.task('deploy', function(callback) {
+    runSequence(
+        'build',
+        'rsync',
+        callback);
+});
+
+gulp.task('build', function(callback) {
+    runSequence(
+        'clean',
+        ['bundle', 'sass', 'views', 'assets'],
+        callback);
+});
 
 gulp.task('clean', function() {
-    del('dist/');
+    return del('dist/');
 });
 
 gulp.task('bundle', function() {
-    gulp.src(['./app/app.js'])
+    return gulp.src(['./app/app.js'])
         .pipe(browserify({
             insertGlobals: true,
             debug: true
@@ -22,19 +36,37 @@ gulp.task('bundle', function() {
 });
 
 gulp.task('sass', function () {
-    gulp.src(['./node_modules/angular-material/angular-material.css', './scss/style.scss'])
+    return gulp.src(['./node_modules/angular-material/angular-material.css', './scss/style.scss'])
         .pipe(sass({sourceComments: 'map'}))
         .pipe(concat('style.css'))
         .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('views', function() {
-    gulp.src('./app/**/*.html')
+    return gulp.src('./app/**/*.html')
         .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('assets', function() {
-    gulp.src('./assets/**/*')
+    return gulp.src('./assets/**/*')
         .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('rsync', function() {
+    return gulp.src('./dist/')
+        .pipe(rsync({
+            destination: '~/frontend',
+            root: '.',
+            hostname: '178.62.158.190',
+            username: 'vagrant',
+            incremental: true,
+            progress: true,
+            relative: true,
+            emptyDirectories: true,
+            recursive: true,
+            clean: true,
+            exclude: ['.DS_Store'],
+            include: []
+        }));
 });
 

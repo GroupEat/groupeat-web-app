9b7563a52dc6e7272ae4f47c95627defc492d55c
Thibault Derousseaux <tib.dex@gmail.com>
2015-03-14 16:46:32 +0100

    Allowing to see the docs on groupeat.dev without having to fill the admin form

--- app/admin/docs.controller.js
+++ app/admin/docs.controller.js
@@ -1,16 +1,29 @@
 /*@ngInject*/
-module.exports = function DocsController(api, $window, authentication) {
+module.exports = function DocsController(api, $window, authentication, $location) {
     var vm = this;
 
-    vm.showDocs = showDocs;
+    vm.logIn = logIn;
 
-    function showDocs() {
+    activate();
+
+    function activate() {
+        if ($location.host() === 'groupeat.dev') {
+            vm.email = 'admin@groupeat.fr';
+            vm.password = 'groupeat';
+            vm.logIn();
+        }
+    }
+
+    function logIn() {
+        var hash = $location.hash();
+
         authentication.logIn(vm.email, vm.password).then(function() {
             api('GET', 'admin/docs').success(function(response) {
-                var  document = $window.document;
+                var document = $window.document;
                 document.open('text/html');
                 document.write(response); // jshint ignore:line
                 document.close();
+                document.getElementById(hash).scrollIntoView();
             });
         });
     }

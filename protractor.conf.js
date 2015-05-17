25ae25cc780a7ee2b8418472f5c647b5ea5f6644
Thibault Derousseaux <tib.dex@gmail.com>
2015-05-17 10:40:24 +0200

    Trying to get Circle CI to work

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,6 @@
 exports.config = {
     baseUrl: 'http://localhost:8010/',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['tests/**/*.js']
+    specs: ['tests/**/*.js'],
+    framework: 'jasmine'
 };

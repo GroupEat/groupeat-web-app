7ab95252fe95364d8cc801244ecea77df424ac32
Thibault Derousseaux <tib.dex@gmail.com>
2015-08-18 22:42:54 +0200

    Improve dashboard responsiveness

--- app/restaurants/dashboard.controller.js
+++ app/restaurants/dashboard.controller.js
@@ -1,7 +1,14 @@
 export default class DashboardController {
-  constructor() {
+  constructor($q, $mdSidenav) {
     'ngInject';
 
+    this.$q = $q;
+    this.$mdSidenav = $mdSidenav;
+
     this.menuItems = ['foo', 'bar', 'baz'];
+  }
+
+  toggleSidenav() {
+    this.$q.when(true).then(() => this.$mdSidenav('left').toggle());
   }
 }

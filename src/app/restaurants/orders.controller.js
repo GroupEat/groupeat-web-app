3c0d169b2fa5a7e84b686187b5c134e875a36836
Thibault Derousseaux <tib.dex@gmail.com>
2015-10-28 16:08:17 +0100

    Add support for socket.io

--- src/app/restaurants/orders.controller.js
+++ src/app/restaurants/orders.controller.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
 import Money from '../support/money.js';
+import io from 'socket.io-client';
 
 export default class OrdersController {
-  constructor(auth, restaurantsService) {
+  constructor($state, auth, restaurantsService) {
     'ngInject';
+
+    const socket = io.connect(`${window.location.origin}:3000`);
+    socket.on('connect', () => {
+      socket.emit('authentication', {token: auth.getToken()});
+      socket.on('authenticated', () => {
+        socket.on('GroupOrderHasBeenCreated', event => {
+          $state.go($state.current, {}, {reload: true});
+        });
+      });
+    });
 
     restaurantsService.getGroupOrders(auth.getUserId()).then(groupOrders => {
       this.groupOrders = _.sortBy(groupOrders.map(groupOrder => {

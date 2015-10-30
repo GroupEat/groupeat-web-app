f062290c49300b686a405f879d8cbbb5d50bbb60
Thibault Derousseaux <tib.dex@gmail.com>
2015-10-30 15:53:31 +0100

    Extract time-ago directive and title filter to separate files

--- src/app/support/module.js
+++ src/app/support/module.js
@@ -4,21 +4,13 @@ import ApiService from './api.service.js';
 import ElementModifierService from './element-modifier.service.js';
 import PopupService from './popup.service.js';
 import SocketService from './socket.service.js';
+import timeAgoDirective from './time-ago.directive.js';
+import titleFilter from './title.filter.js';
 
 angular.module('groupeat.support', ['angular-capitalize-filter'])
   .service('api', ApiService)
   .service('elementModifier', ElementModifierService)
   .service('popup', PopupService)
   .service('socket', SocketService)
-  .directive('timeAgo', () => {
-    return {
-      restrict: 'E',
-      scope: {date: '='},
-      templateUrl: 'support/time-ago.html'
-    };
-  })
-  .filter('title', $filter => {
-    'ngInject';
-
-    return input => $filter('capitalize')($filter('translate')(input), 'first');
-  });
+  .directive('timeAgo', timeAgoDirective)
+  .filter('title', titleFilter);
